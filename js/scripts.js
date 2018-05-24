a06286d3a07fcdc86e1bc30853e4b14875ae9a1e
Cici Harrison and Zoe Zhou <zoe@>
2018-05-23 20:49:13 -0700

    it will use JQuery to tell you your lucky numbers based on your birth year and will display your favorite color in the right column.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 
+
+
   $("form#drag-name").submit(function(event) {
     event.preventDefault();
+
+    var faveCol = $("#color").val();
+
+    var dob = parseInt($("#born").val());
 
     var street = $("#street").val();
     var pet = $("#pet-name").val();
@@ -10,6 +16,13 @@ $(document).ready(function() {
     $(".drag1").append("The "+street+"-"+pet+" Mc"+word);
     $(".drag2").append(pet+"-"+word+street+" III");
     $(".drag3").append("Mc"+word+street+"-"+pet);
+
+    $(".birthday1").prepend(dob/2-47);
+    $(".birthday2").prepend(dob*2+99);
+    $(".birthday3").prepend(dob/5);
+
+    $(".col-md-5.Me").css("background-color", faveCol);
+    console.log(faveCol);
   });
 
 
