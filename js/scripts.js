e7ebaece69e22708dd1b6270c911c89f765a8c80
Cici <cici.harrison@gmail.com>
2018-05-30 15:18:56 -0700

    contain proper formatting in 'Royal Names' section as well as will now show 'Power Numbers' rather than 'Lucky Numbers.'

--- js/scripts.js
+++ js/scripts.js
@@ -13,9 +13,9 @@ $(document).ready(function() {
     var pet = $("#pet-name").val();
     var word = $("#word").val();
 
-    $(".drag1").append("The "+street+"-"+pet+" Mc"+word);
-    $(".drag2").append(pet+"-"+word+street+" III");
-    $(".drag3").append("Mc"+word+street+"-"+pet);
+    $(".drag1").append(street+" "+pet+" Mc"+word);
+    $(".drag2").append(pet+" "+word+ " "+street+" III");
+    $(".drag3").append("Mc"+word+" "+street+" "+pet);
 
     $(".birthday1").prepend(dob/2-47);
     $(".birthday2").prepend(dob*2+99);
