761769706caf0262885c40a905be3689184f02f2
Cici Harrison and Zoe Zhou <zoe@>
2018-06-04 19:58:14 -0700

    it will split birthday into day, month, year and round your power numbers and calculate age of user.

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,22 @@ $(document).ready(function() {
 
     var faveCol = $("#color").val();
 
-    var dob = parseInt($("#born").val());
+    var month = parseInt($("#month").val());
+    var day = parseInt($("#day").val());
+    var year = parseInt($("#year").val());
+    var age = 2018-year;
 
     var street = $("#street").val();
     var pet = $("#pet-name").val();
     var word = $("#word").val();
 
-    $(".drag1").append(street+" "+pet+" Mc"+word);
+    $(".drag1").append(" "+street+" "+pet+" Mc"+word);
     $(".drag2").append(pet+" "+word+ " "+street+" III");
     $(".drag3").append("Mc"+word+" "+street+" "+pet);
 
-    $(".birthday1").prepend(dob/2-47);
-    $(".birthday2").prepend(dob*2+99);
-    $(".birthday3").prepend(dob/5);
+    $(".birthday1").prepend(Math.round(day*month-year+50000));
+    $(".birthday2").prepend(Math.round(year/month+day));
+    $(".birthday3").prepend(Math.round(year/day));
 
     $(".col-md-5.Me").css("background-color", faveCol);
     console.log(faveCol);
